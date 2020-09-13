a9d3ef569ce23e8ecea5fa71656153924b9dec18
Romera <romeramatias0@gmail.com>
2020-09-13 13:54:29 -0300

    Edit Clase-03

--- 2-1-PNT2-Clase-03/proyecto-vue-cdn/js/main.js
+++ 2-1-PNT2-Clase-03/proyecto-vue-cdn/js/main.js
@@ -1,3 +1,21 @@
+Vue.component("contador", {
+   data() {
+      return {
+         cont: Number(this.init) || 0,
+      };
+   },
+   props: ['init', 'color'],
+   methods: {
+      contar() {
+         this.cont++
+      },
+      getColor(){
+         return 'btn-' + this.color
+      }
+   },
+   template: `<button :class="['btn', getColor()]" @click="contar()">Contar {{cont}}</button>`
+});
+
 let app = new Vue({
    /* El es un objeto, representa el selecctor de un elemento al que se va a conectar*/
    el: "#app",
@@ -21,34 +39,33 @@ let app = new Vue({
             apellido: "Romera",
             edad: 24,
             curso: true,
-            foto:
-               "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-256.png",
+            foto: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/7_avatar-256.png",
          },
          {
             nombre: "Federico",
             apellido: "Mateucci",
             edad: 20,
             curso: true,
-            foto:
-               "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-256.png",
+            foto: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/10_avatar-256.png",
          },
          {
             nombre: "Florencia",
             apellido: "Garduno",
             edad: 21,
             curso: true,
-            foto:
-               "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-256.png",
+            foto: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/4_avatar-256.png",
          },
          {
             nombre: "Julian",
             apellido: "Grilli",
             edad: 22,
             curso: false,
-            foto:
-               "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/3_avatar-256.png",
+            foto: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/3_avatar-256.png",
          },
       ],
+      nombre: "Matias",
+      estilo1: true,
+      estilo2: true,
    },
    methods: {
       incrementar() {
@@ -73,8 +90,7 @@ let app = new Vue({
             apellido: "Arguello",
             edad: 23,
             curso: false,
-            foto:
-               "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/1_avatar-256.png",
+            foto: "https://cdn3.iconfinder.com/data/icons/business-avatar-1/512/1_avatar-256.png",
          };
          this.alumnos.push(alu);
       },
@@ -83,20 +99,23 @@ let app = new Vue({
       // Computed se veria como data, se lo puede llamar con alumnosConCurso.xxx
       alumnosConCurso() {
          // Forma 1
-         let cantidad = 0;
-         this.alumnos.forEach(a => {
+         /*          let cantidad1 = 0;
+         this.alumnos.forEach((a) => {
             if (a.curso === true) {
                cantidad++;
             }
-         });
+         }); */
 
          // Forma 2
-         let cantidad2 = this.alumnos.filter(a => a.curso).length;
-
-         return{
-            cantidad : cantidad2, 
-            total: this.alumnos.length
-         }
+         let cantidad2 = this.alumnos.filter((a) => a.curso).length;
+         let totalA = this.alumnos.length;
+         // Computed devuelve un objeto con distintos atributos
+         return {
+            cantidad: cantidad2,
+            total: totalA,
+            ninguno: cantidad2 == 0,
+            todos: cantidad2 == totalA,
+         };
       },
    },
 });
---
+++ 2-1-PNT2-TPS/TP3/app.js
@@ -0,0 +1,52 @@
+new Vue({
+    el: '#app',
+    data: {
+        saludJugador: 100,
+        saludMonstruo: 100,
+        hayUnaPartidaEnJuego: false,
+        turnos: [], //es para registrar los eventos de la partida
+        esJugador: false,
+        rangoAtaque: [3, 10],
+        rangoAtaqueEspecial: [10, 20],
+        rangoAtaqueDelMonstruo: [5, 12],
+    },
+
+    methods: {
+        getSalud(salud) {
+            return `${salud}%`
+        },
+        empezarPartida: function () {
+        },
+        atacar: function () {
+        },
+
+        ataqueEspecial: function () {
+        },
+
+        curar: function () {
+        },
+
+        registrarEvento(evento) {
+        },
+        terminarPartida: function () {
+        },
+
+        ataqueDelMonstruo: function () {
+        },
+
+        calcularHeridas: function (rango) {
+            return 0
+
+        },
+        verificarGanador: function () {
+            return false;
+        },
+        cssEvento(turno) {
+            //Este return de un objeto es prque vue asi lo requiere, pero ponerlo acá queda mucho mas entendible en el codigo HTML.
+            return {
+                'player-turno': turno.esJugador,
+                'monster-turno': !turno.esJugador
+            }
+        }
+    }
+});
